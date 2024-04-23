827f25613363d3a50032cb8d4c0c11617981c218
ScurtisJ <157088615+ScurtisJ@users.noreply.github.com>
2024-04-22 19:01:02 -0500

    Update itemToPurchase.cpp

--- itemToPurchase.cpp
+++ itemToPurchase.cpp
@@ -4,3 +4,18 @@ using namespace std;
 
 #include "ItemToPurchase.h"
 
+Class itemToPurchase{
+  public: 
+    GetName();
+    SetName();
+    SetQuantity();
+      
+    SetName();
+    SetPrice();
+    SetQuantity();
+
+    private:
+      
+}
+
+
